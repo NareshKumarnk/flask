9e0eb643000762c6f0a0b97a882776c93d0d5f76
Naresh <66820548+NareshKumarnk@users.noreply.github.com>
2021-06-13 00:56:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,31 @@
-from flask import Flask, render_template, request, flash
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://naresh:password@postgresql-im0i/postgres'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = 'hi'
-
-db = SQLAlchemy(app)
-
-class People(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    pname = db.Column(db.String(80), unique=True, nullable=False)
-    color = db.Column(db.String(120), nullable=False)
-
-    def __init__(self, id, pname, color):
-        self.id = id
-        self.pname = pname
-        self.color = color
-
-@app.route('/')
-def home():
-    return '<a href="/addperson"><button> Click here </button></a>'
-
-
-@app.route("/addperson")
-def addperson():
-    return render_template("index.html")
-
-
-@app.route("/personadd", methods=['POST'])
-def personadd():
-    id = request.form["id"]
-    pname = request.form["pname"]
-    color = request.form["color"]
-    entry = People(id, pname, color)
-    db.session.add(entry)
-    db.session.commit()
-
-    return render_template("index.html")
-
-
-if __name__ == '__main__':
-    db.create_all()
-    app.run()
+ 
+app.config['MYSQL_HOST'] = 'mysql-30dj'
+app.config['MYSQL_USER'] = 'naresh'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'mysql'
+ 
+mysql = MySQL(app)
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return "Login via the login Form"
+     
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        cursor = mysql.connection.cursor()
+        cursor.execute(''' INSERT INTO test VALUES(%s,%s)''',(name,age))
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
+ 
+app.run(host='localhost', port=5000)
