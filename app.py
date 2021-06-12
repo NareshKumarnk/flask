68cd4c74e1e35006c9b3ddb413259b19b92d7b1d
Naresh <66820548+NareshKumarnk@users.noreply.github.com>
2021-06-13 00:10:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app.secret_key = 'hi'
 
 db = SQLAlchemy(app)
 
+class People(db.Model):
+    pname = db.Column(db.String(80), unique=True, nullable=False)
+    color = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, pname, color):
+        self.pname = pname
+        self.color = color
+
 @app.route('/')
 def home():
     return '<a href="/addperson"><button> Click here </button></a>'
