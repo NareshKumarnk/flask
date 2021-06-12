92940b5a140bbc4a2775eb3d20c480e53a7ff1d1
Naresh <66820548+NareshKumarnk@users.noreply.github.com>
2021-06-12 23:32:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ db = SQLAlchemy(app)
 
 
 class People(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
     pname = db.Column(db.String(80), unique=True, nullable=False)
     color = db.Column(db.String(120), nullable=False)
 
